0b9f4282a6cd1f0e317443408d1aaef940837bcd
Matt Kennedy <mjknj18@gmail.com>
2019-12-15 10:00:22 -0500

    Updated JavaScript Code.

--- static/js/app.js
+++ static/js/app.js
@@ -4,12 +4,15 @@ var tableData = data;
 // Define Function for Table Generation
 function generateTable()
 
+    // Create Table & Column Headers
     {var thead = d3.select("thead").selectAll("th").data(d3.keys(tableData[0])).enter()
-    .append("th").text(function(d){return d});
+    .append("th").text(function(x){return x});
 
+    // Add Rows to Table
     var tr = d3.select("tbody").selectAll("tr").data(tableData).enter().append("tr")
     
-    var td = tr.selectAll("td").data(function(d){return d3.values(d)}).enter().append("td")
-    .text(function(d) {return d})}
+    // Add Cells to Table & Insert Data
+    var td = tr.selectAll("td").data(function(x){return d3.values(x)}).enter().append("td")
+    .text(function(x) {return x})}
 
 generateTable();
