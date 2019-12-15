9d978215323d7fd2e61f4fab98dd702abc5fe83e
Matt Kennedy <mjknj18@gmail.com>
2019-12-15 08:27:49 -0500

    Updated HTML Code.

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,25 @@
-// from data.js
+// Define Table Data from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// TBD
+function myFunction() {
+    // Declare variables
+    var input = document.getElementById(tableData);
+    console.log(input)
+    var filter = input.value.toUpperCase();
+    var table = document.getElementById("myTable");
+    var tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
