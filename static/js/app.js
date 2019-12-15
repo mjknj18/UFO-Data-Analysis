4510ebc10a952141f1ee703e57803582e3cdde75
Matt Kennedy <mjknj18@gmail.com>
2019-12-15 10:21:43 -0500

    Updated JavaScript Code.

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,25 @@
 // Define Table Data from data.js
 var tableData = data;
 
+// Define Function for Filtering Data
+function filterData()
+
+    {console.log(d3.select("#datetime").node().value)}
+
 // Define Function for Table Generation
-function generateTable()
+function generateTable(filteredData)
 
     // Create Table & Column Headers
-    {var thead = d3.select("thead").selectAll("th").data(d3.keys(tableData[0])).enter()
+    {var thead = d3.select("thead").selectAll("th").data(d3.keys(filteredData[0])).enter()
     .append("th").text(function(x){return x});
 
     // Add Rows to Table
-    var tr = d3.select("tbody").selectAll("tr").data(tableData).enter().append("tr")
+    var tr = d3.select("tbody").selectAll("tr").data(filteredData).enter().append("tr")
     
     // Add Cells to Table & Insert Data
     var td = tr.selectAll("td").data(function(x){return d3.values(x)}).enter().append("td")
     .text(function(x) {return x})}
 
-generateTable();
+d3.select("#filter-btn").on("click", filterData())
+
+generateTable(tableData);
