3186a408a711814a0ccbe36632122c03effda0ab
Matt Kennedy <mjknj18@gmail.com>
2019-12-15 09:06:04 -0500

    Updated JavaScript Code.

--- static/js/app.js
+++ static/js/app.js
@@ -1,45 +1,41 @@
 // Define Table Data from data.js
 var tableData = data;
 
-// Fefine Function for Table Generation
+// Define Function for Table Generation
 function generateTable()
 
-    // EXTRACT VALUE FOR HTML HEADER. 
-        // ('Book ID', 'Book Name', 'Category' and 'Price')
-        var col = [];
-        for (var i = 0; i < tableData.length; i++) {
-            for (var key in tableData[i]) {
-                if (col.indexOf(key) === -1) {
-                    col.push(key);
-                }
-            }
-        }
+    // Define Blank List for Header Values
+    {var col = [];
 
-        // CREATE DYNAMIC TABLE.
-        var table = document.createElement("table");
+    //Extract Header Values from Table Data
+    for (var i = 0; i < tableData.length; i++)
+        {for (var key in tableData[i])
+            {if (col.indexOf(key) === -1)
+                {col.push(key);}}}
 
-        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+    // Create Table
+    var table = document.createElement("table");
 
-        var tr = table.insertRow(-1);                   // TABLE ROW.
+    // Add Header Row to Table
+    var tr = table.insertRow(-1);
 
-        for (var i = 0; i < col.length; i++) {
-            var th = document.createElement("th");      // TABLE HEADER.
-            th.innerHTML = col[i];
-            tr.appendChild(th);
-        }
+    // Add Column Headers to Table
+    for (var i = 0; i < col.length; i++)
+        {var th = document.createElement("th");
+        th.innerHTML = col[i];
+        tr.appendChild(th);}
 
-        // ADD JSON DATA TO THE TABLE AS ROWS.
-        for (var i = 0; i < tableData.length; i++) {
+    // Add Rows for Table Data
+    for (var i = 0; i < tableData.length; i++)
+        {tr = table.insertRow(-1);
+        
+        for (var j = 0; j < col.length; j++)
+            {var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = tableData[i][col[j]];}}
 
-            tr = table.insertRow(-1);
+    // Create HTML Container for Table
+    var divContainer = document.getElementById("ufo-table");
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);}
 
-            for (var j = 0; j < col.length; j++) {
-                var tabCell = tr.insertCell(-1);
-                tabCell.innerHTML = tableData[i][col[j]];
-            }
-        }
-
-        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-        var divContainer = document.getElementById("ufo-table");
-        divContainer.innerHTML = "";
-        divContainer.appendChild(table);
+generateTable();
